e3658c3d556d5af400c28faefbdb0eca9fbe50b0
paallaire <pascalandre.allaire@gmail.com>
2023-07-24 18:50:28 -0400

    Tabs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import './styles/main.pcss';
 import { Accordion } from './components/Accordion';
 import { DropdownMenu } from './components/DropdownMenu';
+import { Tabs } from './components/Tabs';
 
 const accordion: Accordion = new Accordion();
 //accordion.openAll(); 
@@ -10,10 +11,18 @@ const accordion: Accordion = new Accordion();
 
 
 // Get the container element
-const dd1 = document.querySelector<HTMLElement>('#dd-options2');
+const dd1 = document.querySelector<HTMLElement>('#dd-options');
 const dd2 = document.querySelector<HTMLElement>('#dd-options-2');
 
 // Create an instance of DropdownMenu
 const dropdown1 = new DropdownMenu(dd1!);
 const dropdown2 = new DropdownMenu(dd2!);
+
+
+
+document.querySelectorAll('.tabs').forEach(element => {
+  console.log('element:', element)
+  const tab = new Tabs(element);
+  console.log('tab:', tab)
+});
 
